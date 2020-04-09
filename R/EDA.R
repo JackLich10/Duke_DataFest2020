8666308dd73ce8c72cd3fdc18a9629f505282163
Jack Lichtenstein <jack.lichtenstein@duke.edu>
2020-04-09 15:31:22 -0400

    added data dictionary and first EDA

--- R/EDA.R
+++ R/EDA.R
@@ -1,1 +1,35 @@
-# bullshit script
+# EDA script
+library(tidyverse)
+library(ggrepel)
+
+USStates <- read_csv("data/Social Distancing - States.csv") %>%
+  mutate(date = as.Date(date, format = "%m/%d/%y"))
+
+is_outlier <- function(x) {
+  return(x <= quantile(x, 0.25) - 1.5 * IQR(x) | x >= quantile(x, 0.75) + 1.5 * IQR(x))
+}
+
+USStates %>%
+  pivot_longer(cols = c("retail_recreation",	"grocery_pharmacy",	"parks",	
+                        "transit_stations",	"workplaces",	"residential"),
+               names_to = "type") %>%
+  group_by(type) %>%
+  mutate(outlier = ifelse(is_outlier(value), value, as.numeric(NA)),
+         high_low = case_when(
+           !is.na(outlier) & outlier > mean(value) ~ "High",
+           !is.na(outlier) & outlier < mean(value) ~ "Low",
+           TRUE ~ as.character(NA))) %>%
+  ggplot() +
+  geom_boxplot(aes(x = reorder(type, -value), y = value/100)) +
+  geom_text_repel(aes(x = reorder(type, -value), y = value/100,
+                      label = ifelse(!is.na(outlier), paste0(state, ": ", outlier, "%"), ""),
+                      color = high_low), size = 3, fontface = "bold") +
+  scale_x_discrete(labels = c("Residential", "Parks", "Grocery/Pharmacy", "Workplaces", "Retail/Recreation", "Transit Stations")) +
+  scale_y_continuous(labels = scales::percent) +
+  scale_color_manual(values = c("red", "blue")) +
+  guides(color = F) +
+  labs(title = "United States Mobility Trends",
+       subtitle = paste0("as of ", USStates$date),
+       x = NULL,
+       y = "%Change in Mobility",
+       caption = "Data courtesy of Google")
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Duke_DataFest2020
+
 2020 Duke DataFest Competition Repo
+
+Data Dictionary:
+
+Number of Observations:
+
+Number of Variables:
+
+Description of Variables:
+
+`country` or `state`: Identifying variable for a particular country or state's google mobility trend data
+
+`date`: Date of community mobility report
+
+Note that for the following variables, each mobility trend is combared to a baseline, defined as the median value, for the corresponding day of the week, during the 5-week period Jan 3–Feb 6, 2020.
+
+`retail_recreation`: Mobility trends for places like restaurants, cafes, shopping centers, theme parks, museums, libraries, and movie theaters.
+
+`grocery_pharmacy`: Mobility trends for places like grocery markets, food warehouses, farmers markets, specialty food shops, drug stores, and pharmacies.
+
+`parks`: Mobility trends for places like national parks, public beaches, marinas, dog parks, plazas, and public gardens.
+
+`transit_stations`: Mobility trends for places like public transport hubs such as subway, bus, and train stations.
+
+`workplaces`: Mobility trends for places of work.
+
+`residential`: Mobility trends for places of residence.
